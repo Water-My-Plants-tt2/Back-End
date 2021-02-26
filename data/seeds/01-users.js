54d0fe5cbdf243d0977ffd267cd931e539e556e2
Nathan Hietala <Nrhietala@gmail.com>
2021-02-26 17:46:38 -0600

    DB/Seeds running locally

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -2,19 +2,19 @@ exports.seed = function (knex) {
   return knex("users").then(function () {
     return knex("users").insert([
       {
-        id: 1,
+        user_id: 1,
         username: "Tommy Tutone",
         password: "comebackjenny",
         phone_number: 8675309,
       },
       {
-        id: 2,
+        user_id: 2,
         username: "Rick Astley",
         password: "neverletyoudown",
         phone_number: 4345508,
       },
       {
-        id: 3,
+        user_id: 3,
         username: "Vanilla Ice",
         password: "i<3chocolate",
         phone_number: 1357988,
