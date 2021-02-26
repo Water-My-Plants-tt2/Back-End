5f8bc2e773a6ccde41f9ad72f14bd9ef54a9721a
Nathan Hietala <Nrhietala@gmail.com>
2021-02-26 16:30:11 -0600

    heroku push

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ const PORT = process.env.NODE_ENV || 5555;
 server.listen(PORT, () => {
   console.log(`\n*** Server listening on ${PORT} ***\n`);
 });
+
+// prior working URL for postgres
+/*
+DEV_DATABASE_URL=postgresql://postgres:natepostgres@localhost/greenhouse
+*/
